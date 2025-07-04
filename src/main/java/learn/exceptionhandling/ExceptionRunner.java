10b205b8955c8082245a251b4afaa654d242b6ca
Sabarinathan G <nsabari86@gmail.com>
2025-07-04 17:32:03 +0530

    added new programs

--- src/main/java/learn/exceptionhandling/ExceptionRunner.java
+++ src/main/java/learn/exceptionhandling/ExceptionRunner.java
@@ -3,14 +3,17 @@ package learn.exceptionhandling;
 public class ExceptionRunner {
     public static void main(String[] args) {
         methodOne();
+        System.out.println("Main Completed");
     }
 
     private static void methodOne() {
         methodTwo();
+        System.out.println("Method One is Completed");
     }
 
     private static void methodTwo() {
         String name = null;
         System.out.println(name.length());
+        System.out.println("Method Two is Completed");
     }
 }
