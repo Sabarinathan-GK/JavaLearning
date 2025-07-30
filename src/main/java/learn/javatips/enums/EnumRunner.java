b49e5639d638dba13a269a8be419490f0dfdd414
Sabarinathan G <nsabari86@gmail.com>
2025-07-30 16:56:57 +0530

    Updated Tips

--- src/main/java/learn/javatips/enums/EnumRunner.java
+++ src/main/java/learn/javatips/enums/EnumRunner.java
@@ -1,15 +1,30 @@
 package learn.javatips.enums;
 
+import java.util.Arrays;
+
 enum Season {
-    SUMMER, WINDER, FALL, SPRING;
+    SPRING(4), SUMMER(1), WINDER(2), FALL(3);
+
+    private int value;
+    private Season(int Value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+
 }
 public class EnumRunner {
     public static void main(String[] args) {
         Season season = Season.FALL;
         System.out.println(season);
-        Season season1 = Season.valueOf("WINDER");
+        Season season1 = Season.valueOf("SUMMER");
         System.out.println(season1);
+        System.out.println(season1.name());
         System.out.println(season1.ordinal());
-
+        System.out.println(Season.SUMMER.getValue());
+        System.out.println(Arrays.toString(Season.values()));
     }
 }
