2da0c077255a3ed9ec66ba5b8279c00ea35a1e65
Sabarinathan G <nsabari86@gmail.com>
2025-09-04 18:41:06 +0530

    Java Latest Features

---
+++ src/main/java/learn/latestfeatures/SwitchWithEnum.java
@@ -0,0 +1,16 @@
+package learn.latestfeatures;
+
+import java.time.DayOfWeek;
+
+import static java.time.DayOfWeek.*;
+
+public class SwitchWithEnum {
+    public static void main(String[] args) {
+        DayOfWeek dayOfWeek = DayOfWeek.FRIDAY;
+        String day = switch(dayOfWeek) -> {
+            case MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY -> "Weekday";
+            case SATURDAY, SUNDAY -> "Weekend";
+        };
+        System.out.println(day);
+    }
+}
