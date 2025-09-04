0b4a6cd67ba1e211ebd6309313e022e8c9ec9b20
Sabarinathan G <nsabari86@gmail.com>
2025-09-04 16:30:24 +0530

    Java Latest Features

---
+++ src/main/java/learn/latestfeatures/NestedPatternMatching.java
@@ -0,0 +1,20 @@
+package learn.latestfeatures;
+
+public class NestedPatternMatching {
+    record Customer(String customerName, String email){}
+    record Product(String productName, double Rate){}
+    record Order(Customer customer, Product product){}
+
+    private static void processOrder(Object obj){
+        if(obj instanceof Order(
+                Customer(String customerName, String email),
+                Product(String productName, double price)
+        )){
+            System.out.println(customerName+"  "+email+"  "+productName+"  "+price);
+        }
+    }
+
+    public static void main(String[] args) {
+        processOrder(new Order(new Customer("Sabari","abc@gmail.com"),new Product("FaceWash",450)));
+    }
+}
---
+++ src/main/java/learn/latestfeatures/PatternMatchingRunner.java
@@ -0,0 +1,40 @@
+package learn.latestfeatures;
+
+public class PatternMatchingRunner {
+
+    record Transaction(String sender, String receiver, int amount){}
+
+    public static void main(String[] args) {
+        Object objString = "Animal";
+        oldVerInstanceOf(objString);
+        newVerInstanceOf(objString);
+        printTransactionNew(new Transaction("Sabari","Mani",1_00_000));
+    }
+    private static void printTransactionNew(Object obj){
+        if(obj
+                instanceof
+                Transaction(String sender, String receiver, int amount)){
+            System.out.println(sender +" "+receiver+" "+amount);
+        }
+    }
+    private static void printTransactionOld(Object obj){
+        if(obj instanceof Transaction trans){
+            String sender = trans.sender();
+            String receiver = trans.receiver();
+            int amt = trans.amount();
+            System.out.println(sender +" "+receiver+" "+amt);
+        }
+    }
+
+    private static void newVerInstanceOf(Object obj) {
+        if(obj instanceof String conString){
+            System.out.println(conString.toUpperCase());
+        }
+    }
+    private static void oldVerInstanceOf(Object obj) {
+        if(obj instanceof String){
+            String convertedString = (String)obj;
+            System.out.println(convertedString.toLowerCase());
+        }
+    }
+}
